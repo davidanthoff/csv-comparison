371ce4816dbc105548d7881941b38be41cbbba6b
David Anthoff <anthoff@berkeley.edu>
2018-10-29 09:14:21 -0700

    Change cols to 20 and 200 cases

--- common.jl
+++ common.jl
@@ -1,6 +1,6 @@
 ourpath(rows, cols, withna) = joinpath("data", string("rows_", rows, "_cols_", cols, "_na_", withna))
 
 const ns = [100, 10_000, 1_000_000];
-const cs = [20, 200, 2000];
+const cs = [20, 200];
 const samples = 5;
 const missing_share = 0.5;
