440071c2c1eef750816fcb3846903ab64e4163c7
David Anthoff <anthoff@berkeley.edu>
2018-11-28 19:49:55 -0800

    Add option to run subset of packages

--- main.jl
+++ main.jl
@@ -5,6 +5,8 @@ using CSV, TextParse, Tables, Printf, RCall, Dates, Queryverse
 import Pandas
 include("common.jl")
 
+const tests_to_run = [:textparse, :csvfiles, :textparse06, :csv, :csv06, :pandas, :rfreads, :rfreadp, :rreadr]
+
 const jl06bin = if Sys.iswindows()
     `$(joinpath(homedir(), "AppData", "Local", "Julia-0.6.4", "bin", "julia.exe"))`
 elseif Sys.isapple()
@@ -26,90 +28,108 @@ function read_specific_file(df, rows, cols, withna, filename, samples)
 
     t::Float64 = 0.
 
-    csvread(filename)
-    for i in 1:samples
-        GC.gc(); GC.gc(); GC.gc()
-        t = @elapsed csvread(filename)
-        push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="TextParse.jl", sample=i, timing=t))
-    end
-
-    DataFrame(load(filename))
-    for i in 1:samples
-        GC.gc(); GC.gc(); GC.gc()
-        t = @elapsed DataFrame(load(filename))
-        push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="CSVFiles.jl", sample=i, timing=t))
-    end
-
-    try
-        ts = parse.(Float64, readlines(`$jl06bin textparsejulia06script.jl $filename $samples`))
-        for (i,t) in enumerate(ts)
-            push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="TextParse.jl; 0.6", sample=i, timing=t))
+    if :textparse in tests_to_run
+        csvread(filename)
+        for i in 1:samples
+            GC.gc(); GC.gc(); GC.gc()
+            t = @elapsed csvread(filename)
+            push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="TextParse.jl", sample=i, timing=t))
         end
-    catch err
-        @info err
     end
 
-    try
-        ts = parse.(Float64, readlines(`$jl06bin csvjulia06script.jl $filename $samples`))
-        for (i,t) in enumerate(ts)
-            push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="CSV.jl; 0.6", sample=i, timing=t))
+    if :csvfiles in tests_to_run
+        DataFrame(load(filename))
+        for i in 1:samples
+            GC.gc(); GC.gc(); GC.gc()
+            t = @elapsed DataFrame(load(filename))
+            push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="CSVFiles.jl", sample=i, timing=t))
         end
-    catch err
-        @info err
     end
 
-    CSV.File(filename) |> DataFrame
-    for i in 1:samples
-        GC.gc(); GC.gc(); GC.gc()
-        t = @elapsed CSV.File(filename) |> DataFrame
-        push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="CSV.jl", sample=i, timing=t))
+    if :textparse06 in tests_to_run
+        try
+            ts = parse.(Float64, readlines(`$jl06bin textparsejulia06script.jl $filename $samples`))
+            for (i,t) in enumerate(ts)
+                push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="TextParse.jl; 0.6", sample=i, timing=t))
+            end
+        catch err
+            @info err
+        end
     end
 
-    Pandas.read_csv(filename)
-    for i in 1:samples
-        GC.gc(); GC.gc(); GC.gc()
-        t = @elapsed Pandas.read_csv(filename)
-        push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="Pandas.jl", sample=i, timing=t))
+    if :csv06 in tests_to_run
+        try
+            ts = parse.(Float64, readlines(`$jl06bin csvjulia06script.jl $filename $samples`))
+            for (i,t) in enumerate(ts)
+                push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="CSV.jl; 0.6", sample=i, timing=t))
+            end
+        catch err
+            @info err
+        end
     end
 
-    R"""
-        fread($filename, nThread=1)
-    """
-    for i in 1:samples
-        t = convert(Float64, R"""
-            start_time = Sys.time()
+    if :csv in tests_to_run
+        CSV.File(filename) |> DataFrame
+        for i in 1:samples
+            GC.gc(); GC.gc(); GC.gc()
+            t = @elapsed CSV.File(filename) |> DataFrame
+            push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="CSV.jl", sample=i, timing=t))
+        end
+    end
+
+    if :pandas in tests_to_run
+        Pandas.read_csv(filename)
+        for i in 1:samples
+            GC.gc(); GC.gc(); GC.gc()
+            t = @elapsed Pandas.read_csv(filename)
+            push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="Pandas.jl", sample=i, timing=t))
+        end
+    end
+
+    if :rfreads in tests_to_run
+        R"""
             fread($filename, nThread=1)
-            end_time = Sys.time()
-            elap = end_time - start_time
-            """)
-        push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="R fread", sample=i, timing=t))
+        """
+        for i in 1:samples
+            t = convert(Float64, R"""
+                start_time = Sys.time()
+                fread($filename, nThread=1)
+                end_time = Sys.time()
+                elap = end_time - start_time
+                """)
+            push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="R fread", sample=i, timing=t))
+        end
     end
 
 
-    R"""
-        fread($filename, nThread=15)
-    """
-    for i in 1:samples
-        t = convert(Float64, R"""
-            start_time = Sys.time()
+    if :rfreadp in tests_to_run
+        R"""
             fread($filename, nThread=15)
-            end_time = Sys.time()
-            elap = end_time - start_time
-            """)
-            push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="R fread parallel", sample=i, timing=t))
+        """
+        for i in 1:samples
+            t = convert(Float64, R"""
+                start_time = Sys.time()
+                fread($filename, nThread=15)
+                end_time = Sys.time()
+                elap = end_time - start_time
+                """)
+                push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="R fread parallel", sample=i, timing=t))
+        end
     end
 
-    R"""
-        suppressMessages(read_csv($filename, progress = FALSE))
-    """
-    for i in 1:samples
-        t = convert(Float64, R"""
-            start_time = Sys.time()
+    if :rreadr in tests_to_run
+        R"""
             suppressMessages(read_csv($filename, progress = FALSE))
-            end_time = Sys.time()
-            elap = end_time - start_time
-            """)
-            push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="R readr", sample=i, timing=t))
+        """
+        for i in 1:samples
+            t = convert(Float64, R"""
+                start_time = Sys.time()
+                suppressMessages(read_csv($filename, progress = FALSE))
+                end_time = Sys.time()
+                elap = end_time - start_time
+                """)
+                push!(df, (file=filename_for_label, rows=rows, cols=cols, withna=withna, package="R readr", sample=i, timing=t))
+        end
     end
 
     nothing
