d773a10358f6694a59de5432a780209e5a2e37a3
David Anthoff <anthoff@berkeley.edu>
2019-03-24 21:51:37 -0400

    Use chunksize=0 for TableReader

--- src/packagescripts/tablereaders_script.jl
+++ src/packagescripts/tablereaders_script.jl
@@ -3,15 +3,15 @@ using TableReader
 warmup_filename = ARGS[1]
 filename = ARGS[2]
 
-t1 = @elapsed(readcsv(warmup_filename))
+t1 = @elapsed(readcsv(warmup_filename, chunksize = 0))
 
 GC.gc(); GC.gc(); GC.gc()
 
-t2 = @elapsed(readcsv(filename))
+t2 = @elapsed(readcsv(filename, chunksize = 0))
 
 GC.gc(); GC.gc(); GC.gc()
 
-t3 = @elapsed(readcsv(filename))
+t3 = @elapsed(readcsv(filename, chunksize = 0))
 
 println(t1)
 println(t2)
