5676c8f4c39a45120b874eb6f75f17ac58f0b61e
David Anthoff <anthoff@berkeley.edu>
2019-05-14 09:15:30 -0700

    Adjust CSV.jl script

--- src/packagescripts/csv_script.jl
+++ src/packagescripts/csv_script.jl
@@ -3,15 +3,15 @@ using CSV
 warmup_filename = ARGS[1]
 filename = ARGS[2]
 
-t1 = @elapsed(CSV.read(warmup_filename))
+t1 = @elapsed(CSV.read(warmup_filename, copycols=true))
 
 GC.gc(); GC.gc(); GC.gc()
 
-t2 = @elapsed(CSV.read(filename))
+t2 = @elapsed(CSV.read(filename, copycols=true))
 
 GC.gc(); GC.gc(); GC.gc()
 
-t3 = @elapsed(CSV.read(filename))
+t3 = @elapsed(CSV.read(filename, copycols=true))
 
 println(t1)
 println(t2)
