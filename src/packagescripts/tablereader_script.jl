1877f40a9a928abedbb908d71c9816edd59bcd68
David Anthoff <anthoff@berkeley.edu>
2019-05-15 10:17:24 -0700

    Set chunkbits=0 for TableReader

--- src/packagescripts/tablereader_script.jl
+++ src/packagescripts/tablereader_script.jl
@@ -3,15 +3,15 @@ using TableReader
 warmup_filename = ARGS[1]
 filename = ARGS[2]
 
-t1 = @elapsed(readcsv(warmup_filename))
+t1 = @elapsed(readcsv(warmup_filename; chunkbits=0))
 
 GC.gc(); GC.gc(); GC.gc()
 
-t2 = @elapsed(readcsv(filename))
+t2 = @elapsed(readcsv(filename; chunkbits=0))
 
 GC.gc(); GC.gc(); GC.gc()
 
-t3 = @elapsed(readcsv(filename))
+t3 = @elapsed(readcsv(filename; chunkbits=0))
 
 println(t1)
 println(t2)
