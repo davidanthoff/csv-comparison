9d036b9f13608a8ad405ade1fb30d5b2343a566a
David Anthoff <anthoff@berkeley.edu>
2019-06-09 10:51:05 -0700

    Use default options for CSV.jl

--- src/packagescripts/csv_script.jl
+++ src/packagescripts/csv_script.jl
@@ -3,15 +3,15 @@ using CSV
 warmup_filename = ARGS[1]
 filename = ARGS[2]
 
-val1, t1, bytes1, gctime1, memallocs1 = @timed(CSV.read(warmup_filename, copycols=true))
+val1, t1, bytes1, gctime1, memallocs1 = @timed(CSV.read(warmup_filename))
 
 GC.gc(); GC.gc(); GC.gc()
 
-val2, t2, bytes2, gctime2, memallocs2 = @timed(CSV.read(filename, copycols=true))
+val2, t2, bytes2, gctime2, memallocs2 = @timed(CSV.read(filename))
 
 GC.gc(); GC.gc(); GC.gc()
 
-val3, t3, bytes3, gctime3, memallocs3 = @timed(CSV.read(filename, copycols=true))
+val3, t3, bytes3, gctime3, memallocs3 = @timed(CSV.read(filename))
 
 println(t1)
 println(t2)
